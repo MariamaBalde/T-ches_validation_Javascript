c35065d903899fc9996777f01f86902368b63b5c
Mariama Balde <foulah18@gmail.com>
2024-06-03 19:32:13 +0000

    adding the increment and decremente section

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 $(".button").each(function () {
   // the containers for all your galleries
   $(this).magnificPopup({
@@ -8,4 +7,47 @@ $(".button").each(function () {
       enabled: true,
     },
   });
+});
+
+document.querySelector("#minus").addEventListener("click", function () {});
+
+let cart = {};
+
+// Décrémenter la valeur de l'input
+document.querySelector("#minus").addEventListener("click", function () {
+  let produit = "produit1";
+  let price = 125.0; //Le prix
+  let quantityInput = document.getElementById(`inpuquantity`);
+  let quantity = parseInt(quantityInput.value);
+  //Décrémenter la valeur de l'input pour diminuer le quantity des produits
+  quantity--;
+  quantityInput.value = quantity;
+  // if (cart[produit]) {
+  //   cart[produit].quantity = quantity;
+  //   if (cart[produit].quantity <= 0) {
+  //     delete cart[produit];
+  //   }
+  // }
+});
+
+// Incrémenter la valeur de l'input
+document.querySelector("#plus").addEventListener("click", function () {
+  let produit = "produit1";
+  let price = 125.0; //Le prix
+  let quantityInput = document.getElementById(`inpuquantity`);
+  let quantity = parseInt(quantityInput.value);
+  //Incrémenter la valeur de l'input pour ajouter le quantity des produits
+  quantity++;
+  quantityInput.value = quantity;
+  // if (cart[produit]) {
+  //   cart[produit].quantity = quantity;
+  // } else {
+  //   cart[produit] = { quantity: quantity, price: price };
+  // }
+});
+
+
+// L'ajout au panier
+document.querySelector("#addtocart").addEventListener("click", function () {
+
 });
